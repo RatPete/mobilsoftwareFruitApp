a1a78b2325d3399680ec70170a3db294ba5b8b47
ratpete <perger.peter.99@gmail.com>
2022-04-08 17:06:13 +0200

    Activity skeletons created

---
+++ android/app/src/main/java/com/example/fruitapp/ui/fruitadd/AddActivity.kt
@@ -0,0 +1,12 @@
+package com.example.fruitapp.ui.fruitadd
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.fruitapp.R
+
+class AddActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add)
+    }
+}
--- android/app/src/main/java/com/example/fruitapp/ui/fruitlist/MainActivity.kt
+++ android/app/src/main/java/com/example/fruitapp/ui/fruitlist/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.fruitapp
+package com.example.fruitapp.ui.fruitlist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.fruitapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
